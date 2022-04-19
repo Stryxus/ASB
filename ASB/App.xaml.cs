b01820693df68692a2f6ec59e57363bfdc6249a2
Connor Shearer <connor_shearer@outlook.com>
2022-04-19 15:57:19 +0100

    Create the MAUI project

---
+++ ASB/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace ASB;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ ASB/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace ASB;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
