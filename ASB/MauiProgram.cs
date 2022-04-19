fb363948bd70cf4f25b3b2e61d09ab6e0a7737ef
Connor Shearer <connor_shearer@outlook.com>
2022-04-19 16:04:10 +0100

    Change font to NotoSans

--- ASB/MauiProgram.cs
+++ ASB/MauiProgram.cs
@@ -9,8 +9,24 @@ public static class MauiProgram
 			.UseMauiApp<App>()
 			.ConfigureFonts(fonts =>
 			{
-				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
-				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+				fonts.AddFont("NotoSans-Black.ttf", "NotoSans-Black");
+				fonts.AddFont("NotoSans-BlackItalic.ttf", "NotoSans-BlackItalic");
+				fonts.AddFont("NotoSans-Bold.ttf", "NotoSans-Bold");
+				fonts.AddFont("NotoSans-BoldItalic.ttf", "NotoSans-BoldItalic");
+				fonts.AddFont("NotoSans-ExtraBold.ttf", "NotoSans-ExtraBold");
+				fonts.AddFont("NotoSans-ExtraBoldItalic.ttf", "NotoSans-ExtraBoldItalic");
+				fonts.AddFont("NotoSans-ExtraLight.ttf", "NotoSans-ExtraLight");
+				fonts.AddFont("NotoSans-ExtraLightItalic.ttf", "NotoSans-ExtraLightItalic");
+				fonts.AddFont("NotoSans-Italic.ttf", "NotoSans-Italic");
+				fonts.AddFont("NotoSans-Light.ttf", "NotoSans-Light");
+				fonts.AddFont("NotoSans-LightItalic.ttf", "NotoSans-LightItalic");
+				fonts.AddFont("NotoSans-Medium.ttf", "NotoSans-Medium");
+				fonts.AddFont("NotoSans-MediumItalic.ttf", "NotoSans-MediumItalic");
+				fonts.AddFont("NotoSans-Regular.ttf", "NotoSans-Regular");
+				fonts.AddFont("NotoSans-SemiBold.ttf", "NotoSans-SemiBold");
+				fonts.AddFont("NotoSans-SemiBoldItalic.ttf", "NotoSans-SemiBoldItalic");
+				fonts.AddFont("NotoSans-Thin.ttf", "NotoSans-Thin");
+				fonts.AddFont("NotoSans-ThinItalic.ttf", "NotoSans-ThinItalic");
 			});
 
 		return builder.Build();
