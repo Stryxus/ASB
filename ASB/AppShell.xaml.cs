8d287147687a0558d4298f33584afb69e9ed9502
Connor Shearer <connor_shearer@outlook.com>
2022-04-30 21:03:34 +0100

    Delete the MAUI project

--- ASB/AppShell.xaml.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ASB;
-
-public partial class AppShell : Shell
-{
-    public AppShell()
-    {
-        InitializeComponent();
-    }
-}
--- ASB/Platforms/MacCatalyst/AppDelegate.cs
+++
@@ -1,9 +0,0 @@
-﻿using Foundation;
-
-namespace ASB;
-
-[Register("AppDelegate")]
-public class AppDelegate : MauiUIApplicationDelegate
-{
-	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
-}
